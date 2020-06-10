8c72eb8f11c14f4cec0bf5ce8a3767d4af058315
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-10 17:34:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,12 @@
 
 
 def fizzbuzz(user_input)
-  if user_input % 3 == 0
-    "Fizz"
+  if user_input % 15 == 0
+    "FizzBuzz"
   elsif user_input % 5 == 0
     "Buzz"
-  elsif user_input % 15 == 0
-    "FizzBuzz"
+  elsif user_input % 3 == 0
+    "Fizz"
   else
     "nil"
   end
